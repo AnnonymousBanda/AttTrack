ff72acf5d4419b8444055bd38d65c7e52baf4b9d
VinayKhedkar <arithmetic.chip@gmail.com>
2026-01-13 17:22:04 +0530

    feat: add verify user route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,16 +12,70 @@ const getUserData = catchAsync(async (req, res) => {
         },
     })
 
-    if (!user) throw new AppError('User not found!', 404, false)
+    if (!user) throw new AppError('User not found!', 404)
 
     res.status(200).json({
-        message: 'Data fetched successfully!',
+        message: 'User data fetched successfully!',
         status: 200,
         data: {
-            name: `${user.first_name} ${user.last_name ?? ''}`.trim(),
-            ...user,
+            user: user,
         },
     })
+})
+
+// Get user data from Microsoft Graph API and check if registered
+const verifyUser = catchAsync(async (req, res) => {
+    await getMicrosoftUserData(req, res)
+    const data = req.microsoftUserData
+    const oid = data.id
+
+    const user = await prisma.users.findUnique({
+        where: {
+            oid: oid,
+        },
+    })
+
+    if (!user) {
+        res.status(404).json({
+            message: 'User not registered!',
+            status: 404,
+            data: {
+                displayName: data.displayName,
+                mail: data.mail,
+                jobTitle: data.jobTitle,
+            },
+        })
+        return
+    }
+
+    res.status(200).json({
+        message: 'User data fetched successfully!',
+        status: 200,
+        data: {
+            user: user,
+        },
+    })
+})
+
+const getMicrosoftUserData = catchAsync(async (req, res) => {
+    const accessToken = req.headers.authorization.split(' ')[1]
+    const response = await fetch('https://graph.microsoft.com/v1.0/me', {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    })
+
+    if (!response.ok) {
+        throw new AppError(
+            'Failed to fetch user data from Microsoft Graph API',
+            500
+        )
+    }
+
+    const data = await response.json()
+
+    req.microsoftUserData = data
 })
 
 const registerUser = catchAsync(async (req, res) => {
@@ -284,4 +338,5 @@ module.exports = {
     modifySemester,
     resetSemester,
     unenrollFromCourse,
+    verifyUser,
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,32 @@
 const router = require('express').Router()
 
-const { getUserData, registerUser, deleteUserData, modifySemester, resetSemester, unenrollFromCourse } = require('../controllers/user.controller')
+const {
+    getUserData,
+    registerUser,
+    deleteUserData,
+    modifySemester,
+    resetSemester,
+    unenrollFromCourse,
+    verifyUser,
+} = require('../controllers/user.controller')
 const { protect } = require('../middleware')
 const validate = require('../middleware/validate')
-const { registerSchema, modifySemesterSchema, unenrollSchema } = require('../utils/validationSchemas')
+const {
+    registerSchema,
+    modifySemesterSchema,
+    unenrollSchema,
+} = require('../utils/validationSchemas')
 
+router.route('/verify').get(verifyUser)
 router.route('/me').get(protect, getUserData)
 router.route('/me').delete(protect, deleteUserData)
 router.route('/register').post(validate(registerSchema), registerUser)
-router.route('/semester').patch(protect, validate(modifySemesterSchema), modifySemester)
+router
+    .route('/semester')
+    .patch(protect, validate(modifySemesterSchema), modifySemester)
 router.route('/semester/reset').post(protect, resetSemester)
-router.route('/course/unenroll').post(protect, validate(unenrollSchema), unenrollFromCourse)
+router
+    .route('/course/unenroll')
+    .post(protect, validate(unenrollSchema), unenrollFromCourse)
 
 module.exports = router
