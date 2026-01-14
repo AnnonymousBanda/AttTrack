5bc8a493cd4a1adbf01b31fc24456cdb2a0226e6
VinayKhedkar <arithmetic.chip@gmail.com>
2026-01-14 19:12:53 +0530

    fix: fix id attribute

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,18 +1,19 @@
-const { prisma } = require("../database");
-const { catchAsync, AppError } = require("../utils/error.util");
+const { prisma } = require('../database')
+const { catchAsync, AppError } = require('../utils/error.util')
 
 const protect = catchAsync(async (req, res, next) => {
-    const uid = req.headers["x-user-id"];
+    const uid = req.headers['x-user-id']
 
-    if(!uid)
-        throw new AppError("Unauthorized Access", 401);
+    if (!uid) throw new AppError('Unauthorized Access', 401)
 
-    const user = await prisma.users.findUnique({ where: { id: uid }, select: { branch: true, semester: true } });
-    if (!user)
-        throw new AppError("User Not Found", 401);
+    const user = await prisma.users.findUnique({
+        where: { id: uid },
+        select: { branch: true, semester: true },
+    })
+    if (!user) throw new AppError('User Not Found', 401)
 
-    req.user={ uid, ...user };
+    req.user = { id: uid, ...user }
     next()
-});
+})
 
-module.exports = { protect };
+module.exports = { protect }
